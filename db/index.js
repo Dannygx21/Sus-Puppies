54a8536178d519e43f47103bb7a93253505de116
Daniel Gonzalez <dboyg123.dg@gmail.com>
2025-12-17 12:32:15 -0500

    init gitignore and cloud database connection

--- db/index.js
+++ db/index.js
@@ -1,25 +1,35 @@
+require('dotenv').config();
+const { DB_URL, DB_USER, DB_PASS, DB_DBNAME } = process.env;
+console.log(process.env)
 //Database Code Goes Here
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://127.0.0.1:27017/susPuppies';
-mongoose.connect(mongoURI);
+mongoose.connect(DB_URL, {
+  dbName: DB_DBNAME,
+  user: DB_USER,
+  pass: DB_PASS,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 
-  const userSchema = new mongoose.Schema({
-    // your code here
-    username: {
-      type: String,
-      required: [true, 'Username is Required'],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is Required'],
-    },
-    socket: {
-      type: String,
-    },
-  });
+const userSchema = new mongoose.Schema({
+  // your code here
+  username: {
+    type: String,
+    required: [true, 'Username is Required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is Required'],
+  },
+  socket: {
+    type: String,
+  },
+});
 
-  const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
